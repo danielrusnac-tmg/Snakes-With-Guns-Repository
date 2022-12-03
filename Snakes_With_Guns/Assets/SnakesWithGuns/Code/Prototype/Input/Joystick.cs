b55089fa640773a6fcebb85998f3ff67e6e504d0
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-12-03 03:11:23 +0200

    Update Joystick.cs

--- Snakes_With_Guns/Assets/SnakesWithGuns/Code/Prototype/Input/Joystick.cs
+++ Snakes_With_Guns/Assets/SnakesWithGuns/Code/Prototype/Input/Joystick.cs
@@ -5,120 +5,103 @@ using UnityEngine.InputSystem.OnScreen;
 
 namespace SnakesWithGuns.Prototype.Input
 {
-	public class Joystick : OnScreenControl
-	{
-		private const float ACTIVE_ALPHA = 1f;
-		private const float INACTIVE_ALPHA = 0.3f;
+    public class Joystick : OnScreenControl
+    {
+        [Header("Settings")]
+        [Range(0f, 1f), SerializeField] private float _radius = 1f;
+        [Range(0f, 1f), SerializeField] private float _activeAlpha = 1f;
+        [Range(0f, 1f), SerializeField] private float _inactiveAlpha = 0.3f;
+        [SerializeField, InputControl(layout = "Vector2")] private string _controlPath;
 
-		[Range(0f, 1f)]
-		[SerializeField] private float _radius = 1f;
-		[SerializeField] private RectTransform _defaultJoystickPosition;
-		[SerializeField] private RectTransform _handle;
-		[SerializeField] private RectTransform _constrain;
-		[SerializeField] private CanvasGroup _canvasGroup;
-		[SerializeField] private Camera _camera;
+        [Header("Components")]
+        [SerializeField] private RectTransform _defaultJoystickPosition;
+        [SerializeField] private RectTransform _handle;
+        [SerializeField] private RectTransform _constrain;
+        [SerializeField] private CanvasGroup _canvasGroup;
 
-		[InputControl(layout = "Vector2")]
-		[SerializeField] private string _controlPath;
+        private Vector2 _startDragPosition;
+        private bool _isActive;
 
-		private Vector2 _startDragPosition;
-		private RectTransform _rectTransform;
-		private bool _isActive;
+        private float ConstrainRadius => _constrain.rect.width * 0.5f * _radius;
 
-		private float ConstrainRadius => _constrain.rect.width * 0.5f * _radius;
+        protected override string controlPathInternal
+        {
+            get => _controlPath;
+            set => _controlPath = value;
+        }
 
-		protected override string controlPathInternal
-		{
-			get => _controlPath;
-			set => _controlPath = value;
-		}
+        private void Awake()
+        {
+            _canvasGroup.alpha = _inactiveAlpha;
+        }
 
-		private void Awake()
-		{
-			_rectTransform = (RectTransform)transform;
-			_canvasGroup.alpha = INACTIVE_ALPHA;
+        private void Start()
+        {
+            ResetAnchors();
+            MoveToDefaultPosition();
+        }
 
-			_constrain.gameObject.SetActive(false);
-		}
+        private void Update()
+        {
+            if (EventSystem.current.currentInputModule.input.GetMouseButtonDown(0))
+            {
+                MoveToActivePosition(EventSystem.current.currentInputModule.input.mousePosition);
+            }
 
-		private void Start()
-		{
-			ResetAnchors();
-			MoveToDefaultPosition();
-		}
+            if (EventSystem.current.currentInputModule.input.GetMouseButton(0))
+            {
+                if (_startDragPosition != EventSystem.current.currentInputModule.input.mousePosition && !_isActive)
+                    _isActive = true;
 
-		private void Update()
-		{
-			if (EventSystem.current.currentInputModule.input.GetMouseButtonDown(0))
-			{
-				MoveToActivePosition(EventSystem.current.currentInputModule.input.mousePosition);
-			}
+                if (_isActive)
+                    OnDrag(EventSystem.current.currentInputModule.input.mousePosition);
+            }
 
-			if (EventSystem.current.currentInputModule.input.GetMouseButton(0))
-			{
-				if (_startDragPosition != EventSystem.current.currentInputModule.input.mousePosition && !_isActive) 
-					_isActive = true;
+            if (EventSystem.current.currentInputModule.input.GetMouseButtonUp(0))
+            {
+                _isActive = false;
+                OnPointerUp();
+            }
+        }
 
-				if (_isActive)
-					OnDrag(EventSystem.current.currentInputModule.input.mousePosition);
-			}
+        private void OnDrag(Vector2 position)
+        {
+            _constrain.gameObject.SetActive(true);
 
-			if (EventSystem.current.currentInputModule.input.GetMouseButtonUp(0))
-			{
-				_isActive = false;
-				OnPointerUp();
-			}
-		}
+            Vector2 direction = Vector2.ClampMagnitude(
+                position - _startDragPosition,
+                ConstrainRadius);
 
-		private void OnDrag(Vector2 position)
-		{
-			_constrain.gameObject.SetActive(true);
+            _handle.localPosition = direction;
+            SendValueToControl(direction / ConstrainRadius);
+        }
 
-			Vector2 direction = Vector2.ClampMagnitude(
-				position - _startDragPosition,
-				ConstrainRadius);
+        private void OnPointerUp()
+        {
+            _constrain.gameObject.SetActive(false);
 
-			_handle.localPosition = direction;
-			SendValueToControl(direction / ConstrainRadius);
-		}
+            MoveToDefaultPosition();
+            SendValueToControl(Vector2.zero);
+        }
 
-		private void OnPointerUp()
-		{
-			_constrain.gameObject.SetActive(false);
+        private void ResetAnchors()
+        {
+            _constrain.anchorMin = new Vector2(0.5f, 0.5f);
+            _constrain.anchorMax = new Vector2(0.5f, 0.5f);
+        }
 
-			MoveToDefaultPosition();
-			SendValueToControl(Vector2.zero);
-		}
+        private void MoveToActivePosition(Vector2 position)
+        {
+            _constrain.position = position;
+            _startDragPosition = position;
+            _canvasGroup.alpha = _activeAlpha;
+        }
 
-		private void ResetAnchors()
-		{
-			_constrain.anchorMin = new Vector2(0.5f, 0.5f);
-			_constrain.anchorMax = new Vector2(0.5f, 0.5f);
-		}
-
-		private void MoveToActivePosition(Vector2 position)
-		{
-			_constrain.position = position;
-			_startDragPosition = position;
-			_canvasGroup.alpha = ACTIVE_ALPHA;
-		}
-
-		private Vector2 ScreenToLocalPosition(Vector2 screenPosition)
-		{
-			RectTransformUtility.ScreenPointToLocalPointInRectangle(
-				_rectTransform,
-				screenPosition,
-				_camera,
-				out Vector2 position);
-
-			return position;
-		}
-
-		private void MoveToDefaultPosition()
-		{
-			_canvasGroup.alpha = INACTIVE_ALPHA;
-			_constrain.position = _defaultJoystickPosition.position;
-			_handle.localPosition = Vector2.zero;
-		}
-	}
+        private void MoveToDefaultPosition()
+        {
+            _canvasGroup.alpha = _inactiveAlpha;
+            _constrain.position = _defaultJoystickPosition.position;
+            _handle.localPosition = Vector2.zero;
+        }
+    }
 }
