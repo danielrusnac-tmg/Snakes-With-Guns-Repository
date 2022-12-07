913d17b5d1c855551dc0c2e37dea720ff1713311
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-12-07 14:05:29 +0200

    Add custom shader

---
+++ Snakes_With_Guns/Assets/SnakesWithGuns/Code/Utilities/WorldCenterSetter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SnakesWithGuns.Utilities
+{
+    public class WorldCenterSetter : MonoBehaviour
+    {
+        private static readonly int WORLD_CENTER = Shader.PropertyToID("_WorldCenter");
+
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            Shader.SetGlobalVector(WORLD_CENTER, _transform.position);
+        }
+    }
+}
