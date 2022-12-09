e74f59294a9b9ed735ee629d22138b782e2d22b4
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-12-09 23:46:08 +0200

    Add difficulty curve

--- Snakes_With_Guns/Assets/SnakesWithGuns/Code/Gameplay/Settings/DummySpawnSettings.cs
+++ Snakes_With_Guns/Assets/SnakesWithGuns/Code/Gameplay/Settings/DummySpawnSettings.cs
@@ -9,16 +9,22 @@ namespace SnakesWithGuns.Gameplay.Settings
     public class DummySpawnSettings
     {
         [SerializeField] private Dummy[] _prefabs;
-        [SerializeField] private float _spawnRate = 2f;
+        [SerializeField] private float _levelDuration = 900f;
+        [SerializeField] private AnimationCurve _dummyChanceCurve = AnimationCurve.Constant(0f, 1f, 1f);
 
         public float CalculateSpawnRate(float gameplayTime)
         {
-            return _spawnRate;
+            return _dummyChanceCurve.Evaluate(NormalizedGameplayTime(gameplayTime));
         }
 
         public Dummy GetPrefab(float gameplayTime)
         {
             return _prefabs[Random.Range(0, _prefabs.Length)];
+        }
+
+        private float NormalizedGameplayTime(float gameplayTime)
+        {
+            return Mathf.Clamp01(gameplayTime / _levelDuration);
         }
     }
 }
